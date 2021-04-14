726008c20d30bb645e3baa363857870d68b17929
pbsmith82 <pbsmith82@gmail.com>
2021-04-14 18:50:18 -0400

    Added Projects

---
+++ db/migrate/20210414224653_create_projects.rb
@@ -0,0 +1,16 @@
+class CreateProjects < ActiveRecord::Migration[6.1]
+  def change
+    create_table :projects do |t|
+      t.string :title
+      t.string :status
+      t.string :target_date
+      t.string :start_date
+      t.string :end_date
+      t.string :project_manager
+      t.string :story_id
+      t.string :project_type_id
+
+      t.timestamps
+    end
+  end
+end
